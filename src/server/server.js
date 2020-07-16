46734b83a5b0031b0e61c40b9eb5a678b60bbb94
Asael Lara <asael.lr.23@gmail.com>
2020-07-16 12:54:32 -0500

    Server side render funcional

---
+++ src/server/server.js
@@ -0,0 +1,91 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+import helmet from 'helmet';
+
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { StaticRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
+import getManifest from './getManifest';
+
+//Busca en todo el entorno un archivo .env y recibe sus variables
+dotenv.config();
+
+const { ENV, PORT } = process.env;
+const app = express();
+
+if (ENV === 'development') {
+  console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const wepackDevMiddleware = require('webpack-dev-middleware');
+  const wepackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
+
+  app.use(wepackDevMiddleware(compiler, serverConfig));
+  app.use(wepackHotMiddleware(compiler));
+
+} else { //MODO PRODUCCION
+  app.use((req, res, next) => {
+    if (!req.hashManifest) req.hashManifest = getManifest();
+    next();
+  });
+  app.use(express.static(`${__dirname}/public`));
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disabled('x-powered-by');
+}
+
+const setResponse = (html, preloadedState, manifest) => {
+  const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
+  const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+  const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
+  return (`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="${mainStyles}" rel="stylesheet" type="text/css">
+      <title>Platzi Video</title>
+  </head>
+  <body>
+      <div id="app">${html}</div>
+      <script type="text/javascript">
+        window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+      </script>
+      <script src="${mainBuild}" type="text/javascript"></script>
+      <script src="${vendorBuild}" type="text/javascript"></script>
+  </body>
+  </html>
+  `
+  );
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const preloadedState = store.getState();
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+  res.send(setResponse(html, preloadedState, req.hashManifest));
+};
+
+app.get('*', renderApp);
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log(`Server running on port ${PORT}`);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,68 @@
 const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+//const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+
+require('dotenv').config();
+
+const isDev = (process.env.ENV === 'development');
+const entry = ['./src/frontend/index.js'];
+
+if (isDev) {
+  entry.push('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true');
+}
 
 module.exports = {
-  entry: './src/index.js',
+  entry,
+  mode: process.env.ENV,
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'src/server/public'),
+    filename: isDev ? 'assets/app.js' : 'assets/app-[hash].js',
     publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: isDev ? 'assets/vendor.js' : 'assets/vendor-[hash].js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some((chunk) => {
+              return chunk.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name);
+            });
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: {
-          loader: 'html-loader',
         },
       },
       {
@@ -54,12 +92,17 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
+    //Ayuda a hacer el refrescado en caliente XD
+    isDev ? new webpack.HotModuleReplacementPlugin() :
+      () => { },
+    isDev ? () => { } :
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$/,
+        filename: '[path].gz',
+      }),
+    isDev ? () => { } : new ManifestPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
+      filename: isDev ? 'assets/app.css' : 'assets/app-[hash].css',
     }),
   ],
 };
